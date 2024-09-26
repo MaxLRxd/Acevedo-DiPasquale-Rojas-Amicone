5f1b35a41bdb5d030bb5ecdfb67c43a5c2547b42
Duende-7 <tiago.sabatier@gmail.com>
2024-09-26 15:50:41 -0300

    commit 2

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('animateBtn').addEventListener('click', function() {
+    const box = document.getElementById('animatedBox');
+    box.style.opacity = '1';
+    box.style.transform = 'translateY(0)';
+});
